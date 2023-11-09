3d1dc64a5389318fc995e8786f10b48775dff142
nai <nai@pop-os.localdomain>
2023-11-09 23:48:03 +0800

    setting up

--- code/settings.py
+++ code/settings.py
@@ -146,12 +146,58 @@ dialog_data = {
 }
 
 monster_data = {
-    'Dwende': {'health': 100,'exp':100,'damage':20,'attack_type': 'slash', 'attack_sound':'../audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 360},
-    'Engkanto': {'health': 300,'exp':250,'damage':40,'attack_type': 'claw',  'attack_sound':'../audio/attack/claw.wav','speed': 2, 'resistance': 3, 'attack_radius': 120, 'notice_radius': 400},
-    'Kapre': {'health': 100,'exp':110,'damage':8,'attack_type': 'thunder', 'attack_sound':'../audio/attack/fireball.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 60, 'notice_radius': 350},
-    #'Tikbalang': {'health': 70,'exp':120,'damage':6,'attack_type': 'leaf_attack', 'attack_sound':'../audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 50, 'notice_radius': 300},
-	'Tiyanak': {'health': 70,'exp':120,'damage':6,'attack_type': 'leaf_attack', 'attack_sound':'../audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 50, 'notice_radius': 300}
+    'Dwende': {
+        'health': 100,
+        'exp':100,
+        'damage':20,
+        'attack_type': 'slash',
+        'attack_sound':'../audio/attack/slash.wav',
+        'speed': 3,
+        'resistance': 3,
+        'attack_radius': 80,
+        'notice_radius': 360},
+    'Engkanto': {
+        'health': 300,
+        'exp':250,
+        'damage':40,
+        'attack_type': 'claw',
+        'attack_sound':'../audio/attack/claw.wav',
+        'speed': 2,
+        'resistance': 3,
+        'attack_radius': 120,
+        'notice_radius': 400},
+    'Kapre': {
+        'health': 100,
+        'exp':110,
+        'damage':8,
+        'attack_type': 'thunder',
+        'attack_sound':'../audio/attack/fireball.wav',
+        'speed': 4, 'resistance': 3, 'attack_radius': 60, 'notice_radius': 350},
+
+	'Tiyanak': {
+        'health': 70,
+        'exp':120,
+        'damage':6,
+        'attack_type': 'leaf_attack',
+        'attack_sound':'../audio/attack/slash.wav',
+        'speed': 3,
+        'resistance': 3,
+        'attack_radius': 50,
+        'notice_radius': 300}
 }
+
+
+"""     'Tikbalang': {
+        'health': 70,
+        'exp':120,
+        'damage':6,
+        'attack_type': 'leaf_attack',
+        'attack_sound':'../audio/attack/slash.wav',
+        'speed': 3,
+        'resistance': 3,
+        'attack_radius': 50,
+        'notice_radius': 300}, """
+
 
 #enemy effect
 """ 
